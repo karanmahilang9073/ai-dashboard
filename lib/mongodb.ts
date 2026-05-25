5a7296a5d2ab6ee667d86dd2447f9abf6633cf01
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-25 17:10:58 +0530

    mongodb deployment code improved

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,23 +1,37 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
+
 if (!MONGODB_URI) {
-    throw new Error('MONGODB_URI not defined')
+  throw new Error("MONGODB_URI not defined");
 }
 
-let cached = (global as any).mongoose
+type MongooseCache = {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+};
 
-if(!cached){
-    cached = (global as any).mongoose = {conn: null, promise: null}
+declare global {
+  var mongooseCache: MongooseCache | undefined;
 }
+
+const cached =
+  global.mongooseCache ??
+  (global.mongooseCache = {
+    conn: null,
+    promise: null,
+  });
 
 export async function connectDB() {
-    if(cached.conn){
-        return cached.conn
-    }
-    if(!cached.promise){
-        cached.promise = mongoose.connect(MONGODB_URI)
-    }
-    cached.conn = await cached.promise
-    return cached.conn
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!);
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
