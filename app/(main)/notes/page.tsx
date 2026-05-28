b6ba4c2efd514f6b5c6f5ce976054643cf3cae8d
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-28 15:51:45 +0530

    form toggler added

--- app/(main)/notes/page.tsx
+++ app/(main)/notes/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react"
 
 function Notespage() {
-  const [notes, setNotes] = useState<{_id: string, title: string, content: string, attachments: {url: string, filename: string}[]}[]>([])
+  const [notes, setNotes] = useState<{_id: string, title: string, content: string, attachments: {url: string, filename: string}[], createdAt: string, updatedAt: string}[]>([])
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
   const [url, setUrl] = useState("")
@@ -14,7 +14,10 @@ function Notespage() {
 
   const [search, setSearch] = useState("")
 
+  const [showForm, setShowForm] = useState(false)
+
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/immutability
     fetchNotes()
   },[])
 
@@ -105,19 +108,32 @@ function Notespage() {
     <div className="p-6">
         <h1 className="text-3xl font-bold">Notes</h1>
 
-        <div className="border p-4 mb-6 rounded">
-          <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} className="border p-2 w-full mb-2" />
-          <textarea placeholder="content" value={content} onChange={(e) => setContent(e.target.value)} className="border p-2 w-full mb-2"></textarea>
-          <input type="file" onChange={upload} className="border p-2 w-full mb-2" key={url} />
-          <button onClick={handleCreate} disabled={uploading} className="bg-black text-white p-2 rounded">
-            Create Note
-          </button>
-        </div>
+        {showForm && (
+          <div className="border p-4 mb-6 rounded">
+            <div className="border p-4 mb-6 rounded">
+              <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} className="border p-2 w-full mb-2" />
+              <textarea placeholder="content" value={content} onChange={(e) => setContent(e.target.value)} className="border p-2 w-full mb-2"></textarea>
+              <input type="file" onChange={upload} className="border p-2 w-full mb-2" key={url} />
+              <div className="flex gap-3">
+                <button onClick={handleCreate} disabled={uploading} className="bg-green-500 hover:bg-green-600 text-white p-2 rounded">
+                  Create Note
+                </button>
+                <button onClick={() => setShowForm(false)} className="bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded">
+                  cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        <button onClick={() => setShowForm(true)} className="bg-blue-400 px-4 py-2 text-white hover:bg-blue-600 rounded mb-4">
+          create note
+        </button>
 
         <input type="text" placeholder="search notes..." value={search} onChange={(e) => setSearch(e.target.value)} className="border p-2 w-full mb-4 rounded-3xl" />
 
         <div>
-          {notes.filter(note => note.title.toLowerCase().includes(search.toLowerCase()) || note.content.toLowerCase().includes(search.toLowerCase())).map((note: {_id: string, title: string, content: string, attachments: {url: string, filename: string}[]}) => (
+          {notes.filter(note => note.title.toLowerCase().includes(search.toLowerCase()) || note.content.toLowerCase().includes(search.toLowerCase())).map((note: {_id: string, title: string, content: string, attachments: {url: string, filename: string}[], createdAt: string, updatedAt: string}) => (
             
             editId === note._id ? (
               <div key={note._id} className="border p-4 mb-2 rounded">
@@ -132,8 +148,12 @@ function Notespage() {
                 <h2 className="font-bold">{note.title}</h2>
                 <p>{note.content}</p>
                 {note.attachments && note.attachments[0] && (
+                  // eslint-disable-next-line @next/next/no-img-element
                   <img src={note.attachments[0].url} alt="attachment" className="w-60 mt-2 rounded" />
                 )}
+                <div className="text-xs text-gray-500 mt-2">
+                    createdAt: {new Date(note.createdAt).toLocaleDateString()}
+                </div>
               </div>
               <div className="flex gap-2">
                 <button onClick={() => handleEdit(note._id, note.title, note.content)} className="bg-blue-500 text-white p-2 rounded">Edit</button>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   }
   
   return (
-    <nav className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-50">
+    <nav className="bg-gray-200 border-b border-gray-200  shadow-sm sticky top-0 z-50">
       <div className="flex justify-between items-center px-6 py-4">
         <h2 className="text-2xl font-bold text-gray-900">AI Dashboard</h2>
         <button onClick={handleLogout} className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 transition-colors font-medium">Logout</button>
